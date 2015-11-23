7f9039dd1e455361879ef2a0da29e9102115e4d6
CaymanSeb <sebastian.bluefire@freenet.de>
2015-11-23 13:46:50 +0100

    Create integer_operations.cpp

---
+++ integer_operations.cpp
@@ -0,0 +1,25 @@
+#include "stdafx.h"
+#ifndef DOPPEL
+#define DOPPEL
+#include "integer_operations.h"
+#include <math.h>
+
+int max_int(int d, int e, int f) {
+	
+	int w = fmax(d, e);
+
+	return fmax(w, f);
+}
+
+int min_int(int g, int h, int i) {
+
+	int q = fmin(g, h);
+
+	return fmin(q, i);
+}
+
+int sum_int(int a, int b, int c) {
+
+	return a + b + c;
+}
+#endif
