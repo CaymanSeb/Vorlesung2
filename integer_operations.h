171bc0d35ababa18afaa2bba40ca5785bc799b5b
CaymanSeb <sebastian.bluefire@freenet.de>
2015-11-23 13:46:13 +0100

    Create integer_operations.h

---
+++ integer_operations.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "integer_operations.cpp"
+
+// Errechne Maximalwert dreier ganzer Zahlen
+int max_int(int, int, int);
+
+// Errechne Minimalwert dreier ganzer Zahlen
+int min_int(int, int, int);
+
+// Errechne Summe dreier ganzer Zahlen
+int sum_int(int, int, int);
